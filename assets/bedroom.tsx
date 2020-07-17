06936b811ffd2f46e38b35762f2af8d53bbaaf85
Aaron Ho <aayho@ucsc.edu>
2020-07-17 10:58:23 -0700

    cutscene finished but its within the game scene

--- assets/bedroom.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="bedroom" tilewidth="70" tileheight="70" tilecount="16" columns="4">
- <image source="bedroom.png" width="280" height="280"/>
-</tileset>

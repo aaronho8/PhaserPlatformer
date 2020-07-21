17a35698e46106892ca1f16bf131201b676e1eb8
Aaron Ho <aayho@ucsc.edu>
2020-07-21 11:18:45 -0700

    new background for level 2

--- src/scenes/Game1.js
+++ src/scenes/Game1.js
@@ -12,9 +12,8 @@ class Game1 extends Phaser.Scene {
       this.load.spritesheet('tiles', 'assets/tiles.png', {frameWidth: 70, frameHeight: 70});
       
       // sky assets
-      this.load.image('sun', './assets/sun_shiny.png');
-      this.load.image('bigCloud', './assets/big_cloud.png');
-      this.load.image('smallCloud', './assets/Cloud.png');
+      this.load.image('volcano', './assets/volcano.png');
+      
 
       // player/sprite animations and animations
       this.load.atlas('player', 'assets/player.png', 'assets/player.json');
@@ -45,17 +44,14 @@ class Game1 extends Phaser.Scene {
       //  replace with sky blue
       this.add.rectangle(0, 0, 2200, 540, 0xFF4500).setOrigin(0, 0);
       this.add.rectangle(0, 500, 2200, 200, 0x9b7653).setOrigin(0, 0);
-      this.add.image(-350, -300, 'sun').setScale(3).setOrigin(0,0);
-      this.add.image(80, 50, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(200, 100, 'smallCloud').setScale(3).setOrigin(0,0);
-      this.add.image(360, 80, 'smallCloud').setScale(3).setOrigin(0,0);
-      this.add.image(500, 100, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(750, 80, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(1200, 100, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(1300, 50, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(1400, 80, 'smallCloud').setScale(3).setOrigin(0,0);
-      this.add.image(1500, 50, 'bigCloud').setScale(3).setOrigin(0,0);
-      this.add.image(1700, 80, 'bigCloud').setScale(3).setOrigin(0,0);
+      
+      this.add.image(0, 0, 'volcano').setScale(3).setOrigin(0,0);
+      this.add.image(500, 0, 'volcano').setScale(3).setOrigin(0,0);
+
+      this.add.image(1000, 0, 'volcano').setScale(3).setOrigin(0,0);
+      this.add.image(1500, 0, 'volcano').setScale(3).setOrigin(0,0);
+
+
 
       // load the map 
       map = this.make.tilemap({key: 'level2'});

e9dcdd6526d6eadc1729922642668152a8e87269
Justin Lao <jlao3@ucsc.edu>
2020-07-22 12:45:33 -0700

    Update Winner.js

--- src/scenes/Winner.js
+++ src/scenes/Winner.js
@@ -2,4 +2,9 @@ class End extends Phaser.Scene {
     constructor() {
       super("endScene");
     }
+
+    preload() {
+        this.load.image('player1', 'assets/player1.png');
+        this.load.image('girl', './assets/girl.png');
+    }
 }
